71f5850e58890f31b8f3d8f5b9aa67f550e560e0
Alireza Sadeghi <ithoughtful@gmail.com>
2024-09-30 17:07:39 +0330

    add run script for hourly data serialisation

---
+++ run_serialise_raw_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from data_lake_transformer import DataLakeTransformer
+from datetime import datetime, timedelta
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+  try:
+    transformer = DataLakeTransformer(dataset_base_path='gharchive/events')
+    now = datetime.utcnow()
+    # Calculate the process_date (2 hours before to ensure ingestion is run)
+    process_date = now.replace(minute=0, second=0, microsecond=0) - timedelta(hours=2)
+    transformer.serialise_raw_data(process_date)
+    logging.info(f"Successfully serializsd raw data for {process_date}")
+  except Exception as e:
+    logging.error(f"Error in serialise_raw_data: {str(e)}")
+
+if __name__ == "__main__":
+  main()
